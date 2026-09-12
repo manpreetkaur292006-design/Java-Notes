2b1e30976af9dc44f9142058ec8495777bf0326d
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-12 11:20:59 +0530

    Added the notes for the break and continue statements

--- src/Break_and_Continue.java
+++ src/Break_and_Continue.java
@@ -1,2 +1,21 @@
 public class Break_and_Continue {
+    public static void main(String[] args){
+
+        // break = break out of a loop (STOP)
+        // continue = skip current iteration of a loop (SKIP)
+
+        for (int i=0; i<10; i++) {
+
+            if (i == 7) { // break the loop when i = 7
+                break;
+            }
+
+            if (i==5){  // skip the iteration when i = 5
+                continue;
+            }
+
+            System.out.print(i+ " ");
+
+        }
+    }
 }
