461c38cff83a96556ef87f7de8ffbe5c55e3bfe1
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-12 11:22:41 +0530

    Added the code for the number guessing game

--- src/Number_guessing_game.java
+++ src/Number_guessing_game.java
@@ -1,2 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Number_guessing_game {
+    public static void main(String[] args) {
+
+        // NUMBER GUESSING GAME
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts=0;
+        int min = 1;
+        int max = 100;
+        // generating random numbers from 1 to 100
+        int randomNumber = random.nextInt(min, max+1);
+
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n ",min,max);
+
+        do{
+            System.out.println("Enter a guess: ");
+            guess=scanner.nextInt();
+            attempts++;
+
+            // giving hints to user to guess the number
+            if(guess<randomNumber){
+                System.out.println("TOO LOW ! Try again");
+            }
+
+            else if (guess>randomNumber) {
+                System.out.println("TOO HIGH ! Try again");
+            }
+
+            else{
+                System.out.println("CORRECT ! The number was "+randomNumber);
+                System.out.println("Number of attempts: "+attempts);
+            }
+
+        }while(guess != randomNumber);
+
+        scanner.close();
+
+    }
 }
