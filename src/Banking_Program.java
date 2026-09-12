a8008433a90fb521005d43d1dac2893d056af504
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-12 16:17:29 +0530

    Added the code for the mini project banking program

--- src/Banking_Program.java
+++ src/Banking_Program.java
@@ -1,2 +1,94 @@
+import java.util.Scanner;
+
 public class Banking_Program {
+
+    static Scanner scanner = new Scanner(System.in);
+    // class scope variable to avoid making this variable multiple times
+
+    public static void main(String[] args){
+
+        // BANKING PROGRAM - MINI PROJECT
+
+        // DECLARE VARIABLES
+
+        double balance=0;
+        boolean isRunning = true;
+        int choice;
+
+        // while loop
+
+        while (isRunning) {
+
+            // DISPLAY A MENU
+
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
+
+            // GET AND PROCESS USERS CHOICE
+
+            System.out.print("Enter your choice (1-4): ");
+            choice=scanner.nextInt();
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance+=deposit();
+                case 3 -> balance-=withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE!");
+
+            }
+        }
+
+        // EXIT MESSAGE
+
+        System.out.println("***************************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************************");
+
+        scanner.close();
+
+    }
+
+    // showBalance() METHOD
+    static void showBalance(double balance){
+        System.out.println("***************");
+        System.out.printf("$ %.2f\n",balance);
+    }
+
+    // deposit() METHOD
+    static double deposit(){
+        double amount;
+        System.out.print("Enter an amount to be deposited: ");
+        amount=scanner.nextDouble();
+        if(amount<0){
+            System.out.println("Amount cannot be negative");
+            return 0;
+        }
+        else{
+            return amount;
+        }
+    }
+
+    // withdraw() METHOD
+    static double withdraw(double balance){
+        double amount;
+        System.out.print("Enter amount to be withdrawn: ");
+        amount=scanner.nextDouble();
+        if(amount>balance){
+            System.out.println("INSUFFICIENT FUNDS");
+            return 0;
+        } else if (amount<0) {
+            System.out.println("Amount cannot be negative");
+            return 0;
+        }
+        else{
+            return amount;
+        }
+    }
+
 }
