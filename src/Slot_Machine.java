94ffca4a86eda6a0c7e7a77a83711ede36b0b069
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-13 16:09:56 +0530

    Added the code for the slot machine mini project

--- src/Slot_Machine.java
+++ src/Slot_Machine.java
@@ -1,2 +1,152 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Slot_Machine {
+    public static void main(String[] args){
+
+        // SLOT MACHINE
+
+        // DECLARE VARIABLES
+
+        Scanner scanner = new Scanner(System.in);
+
+        int balance = 100;
+        int bet;
+        int payout;  // how much money do we win
+        String[] row;
+        String playAgain;
+
+        // DISPLAY A WELCOME MESSAGE
+
+        System.out.println("************************");
+        System.out.println(" Welcome to Java Slots! ");
+        System.out.println("Symbols: 🍕 🍔 🍟 🌭 🍿");
+        System.out.println("************************");
+
+        // PLAY IF BALANCE > 0
+
+        while (balance>0) {
+
+            // ENTER BET AMOUNT
+
+            System.out.println("Current balance: $ "+balance);
+            System.out.print("Place your bet amount: ");
+
+            bet=scanner.nextInt();
+            scanner.nextLine();
+
+            // - VERIFY IF BET > BALANCE
+
+            if (bet>balance){
+                System.out.println("INSUFFICIENT FUNDS");
+                continue;
+            }
+
+            // - VERIFY IF BET > 0
+
+            else if (bet<=0) {
+                System.out.println("Bet must be greater than zero");
+                continue;
+            }
+
+            // - SUBTRACT BET FROM BALANCE
+
+            else{
+                balance-=bet;
+            }
+
+            System.out.println("Spinning...");
+
+            row=spinRow();
+
+            printRow(row);
+
+            payout = getPayout(row,bet);
+
+            if (payout>0){
+                System.out.println("You won $ "+payout);
+                balance+=payout;
+            }
+            else{
+                System.out.println("Sorry You Lost this round !");
+            }
+
+            // ASK TO PLAY AGAIN
+
+            System.out.print("Do you want to play again? (Y/N) : ");
+            playAgain=scanner.nextLine().toUpperCase();
+
+            if (!playAgain.equals("Y")) {
+                break;
+            }
+        }
+
+        // DISPLAY EXIT MESSAGE
+
+        System.out.println("GAME OVER! Your final balance is : $ "+balance);
+
+        scanner.close();
+
+    }
+
+    // SPIN THE ROW
+
+    static String[] spinRow(){
+
+        String[] symbols = {"🍕","🍔","🍟","🌭","🍿"};
+        String[] row = new String[3]; // empty array of length 3
+
+        Random random = new Random();
+
+        for (int i=0; i<3; i++){
+            row[i]=symbols[random.nextInt(symbols.length)];
+        }
+
+        // if your range starts from zero than you don't need to
+        // write the starting value in side the nextInt method technically
+
+        return row;
+    }
+
+    // PRINT ROW
+
+    static void printRow(String[] row){
+
+        System.out.println("**************");
+        System.out.println(" "+String.join(" | ",row));
+        System.out.println("**************");
+
+        // for every string if our string array is row , this will
+        // join them with a " | "
+    }
+
+    // GET PAYOUT
+
+    static int getPayout(String[] row,int bet){
+
+        if (row[0].equals(row[1]) && row[1].equals(row[2])){
+            return switch (row[0]) {
+                case "🍕" -> bet*3;
+                case "🍔" -> bet*4;
+                case "🍟" -> bet*5;
+                case "🌭" -> bet*10;
+                case "🍿" -> bet*20;
+                default -> 0;
+            };
+        }
+
+        else if (row[0].equals(row[1]) || row[1].equals(row[2])){
+            return switch (row[0]) {
+                case "🍕" -> bet*2;
+                case "🍔" -> bet*3;
+                case "🍟" -> bet*4;
+                case "🌭" -> bet*5;
+                case "🍿" -> bet*10;
+                default -> 0;
+            };
+        }
+
+        return 0;
+
+    }
 }
