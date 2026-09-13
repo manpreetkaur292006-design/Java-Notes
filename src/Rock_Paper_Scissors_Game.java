34c85ff3af0a5ad953b48f09f59dc3cc715d73e2
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-13 14:48:30 +0530

    Added the code for the rock paper scissors game

--- src/Rock_Paper_Scissors_Game.java
+++ src/Rock_Paper_Scissors_Game.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Rock_Paper_Scissors_Game {
@@ -8,17 +9,58 @@ public class Rock_Paper_Scissors_Game {
         // DECLARE VARIABLES
 
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        String[] choices = {"rock","paper","scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
 
         // GET A CHOICE FROM THE USER
 
-        // GET RANDOM CHOICE FOR THE COMPUTER
+        do{
+            System.out.print("Enter your move (rock,paper,scissors): ");
+            playerChoice = scanner.nextLine().toLowerCase();
 
-        // CHECK WIN CONDITIONS
+            if (!playerChoice.equals("rock") &&
+                    !playerChoice.equals("paper") &&
+                    !playerChoice.equals("scissors")) {
+                System.out.println("Invalid Choice!");
+                continue;
+            }
 
-        // ASK TO PLAY AGAIN ?
+            // GET RANDOM CHOICE FOR THE COMPUTER
+
+            computerChoice = choices[random.nextInt(3)];
+            // return a random number between 0 and 2
+            System.out.println("Computer Choice: " + computerChoice);
+
+            // CHECK WIN CONDITIONS
+
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("It's a tie!");
+            }
+            // here we have condensed the three else if statements in to the one
+            else if ((playerChoice.equals("rock") && computerChoice.equals("scissors") ||
+                    (playerChoice.equals("paper") && computerChoice.equals("rock")) ||
+                    playerChoice.equals("scissors") && computerChoice.equals("paper"))) {
+                System.out.println("You win!");
+            } else {
+                System.out.println("You lose!");
+            }
+
+            // ASK TO PLAY AGAIN ?
+
+            System.out.print("Play again (yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+
+        }while(playAgain.equals("yes"));
 
         // GOOD BYE MESSAGE
+
+        System.out.println("Thanks for playing!");
+
+        scanner.close();
 
     }
 }
