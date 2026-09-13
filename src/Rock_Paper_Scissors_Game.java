4e483f268a7a8d3bb2c22eb0cf68ab10de43d545
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-13 10:44:56 +0530

    Added the file for the mini project rock paper and scissors

---
+++ src/Rock_Paper_Scissors_Game.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Rock_Paper_Scissors_Game {
+    public static void main(String[] args){
+
+        // ROCK-PAPER-SCISSORS GAME
+
+        // DECLARE VARIABLES
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        // GET A CHOICE FROM THE USER
+
+        // GET RANDOM CHOICE FOR THE COMPUTER
+
+        // CHECK WIN CONDITIONS
+
+        // ASK TO PLAY AGAIN ?
+
+        // GOOD BYE MESSAGE
+
+    }
+}
