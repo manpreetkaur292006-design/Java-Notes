8e0857c1056be3c4d94f1d091adbbe2526cb2f87
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-09-20 23:57:07 +0530

    Added the definition for writing files

--- src/Write_Files.java
+++ src/Write_Files.java
@@ -1,2 +1,12 @@
 public class Write_Files {
+    public static void main(String[] args){
+
+        // How to write a file using Java (4 popular options)
+
+        // FileWriter = Good for small or medium-sized text files
+        // BufferedWriter = Better performance for large amounts of text
+        // PrintWriter = Best for structured data, like reports or logs
+        // FileOutputStream = Best for binary files (e.g., images, audio files)
+
+    }
 }
